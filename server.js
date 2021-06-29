bbda4e9e8870bf45517c48b6137546c8563a357e
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-06-29 13:42:29 -0500

    add express

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
-const http = require('http');
-const querystring = require('querystring');
+const express = require('express');
 
 const port = process.env.PORT || 8080;
+
+const app = express();
 
 const respondText = (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
@@ -10,8 +11,7 @@ const respondText = (req, res) => {
 }
 
 const respondJson = (req, res) => {
-    res.setHeader('Content-Type', 'applicaiton/json');
-    res.end(JSON.stringify({ message: 'Riddle me this Batman.', luckyNumbers: [42, 3, 27]}));
+    res.json({ text: 'Riddle me this Batman.', luckyNumbers: [42, 3, 27]});
 }
 
 const respondNotFound = (req, res) => {
@@ -19,43 +19,30 @@ const respondNotFound = (req, res) => {
     res.end('Not Found');
 }
 
+const respondEcho = (req, res) => {
+    const { input = '' } = req.query;
+
+    res.json({
+        normal: input,
+        shouty: input.toUpperCase(),
+        characterCount: input.length,
+        backwards: input
+            .split('')
+            .reverse()
+            .join('')
+    })
+}
+
 const respondStatic = (req, res) => {
-    const filename = `${__dirname}/public${req.url.split('/static')[1]}`;
+    const filename = `${__dirname}/public/${req.params[0]}`
     fs.createReadStream(filename)
         .on('error', () => respondNotFound(req, res))
-        .pipe(res);
+        .pipe(res)
 }
 
-const respondEcho = (req, res) => {
-    const { input = '' } = querystring.parse(
-        req.url
-            .split('?')
-            .slice(1)
-            .join('')
-    )
+app.get('/', respondText);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+app.get('/static/*', respondStatic);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end(
-        JSON.stringify({
-            normal: input,
-            shouty: input.toUpperCase(),
-            characterCount: input.length,
-            backwards: input
-                .split('')
-                .reverse()
-                .join('')
-        })
-    )
-}
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/') return respondText(req, res);
-    if (req.url === '/message') return respondJson(req, res);
-    if (req.url.match(/^\/echo/)) return respondEcho(req, res);
-    if (req.url.match(/^\/static/)) return respondStatic(req, res)
-
-    respondNotFound(req, res);
-  });
-
-server.listen(port);
-console.log(`Server listening on port ${port}`);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
